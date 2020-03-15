9a05d8427d936dabcc4d8cea57086b8df3095040
Goomie <remy.manescau@gmail.com>
2020-03-15 02:50:17 +0100

    add login to instagram

---
+++ main.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+from time import sleep
+from secrets import *
+
+class InstaBot:
+    def __init__(self, username, password):
+
+        self.driver = webdriver.Chrome()
+        self.username = username
+
+        # Open instagram with ChromeDriver
+        self.driver.get("https://www.instagram.com")
+        sleep(2)
+
+        # Connect to instagram with login and password
+        self.driver.find_element_by_xpath("//input[@name=\"username\"]")\
+            .send_keys(username)
+        self.driver.find_element_by_xpath("//input[@name=\"password\"]")\
+            .send_keys(password)
+        self.driver.find_element_by_xpath('//button[@type="submit"]')\
+            .click()
+        sleep(4)
+
+        # Turn notifications off
+        self.driver.find_element_by_xpath("//button[contains(text(), 'Plus tard')]")\
+            .click()
+        sleep(2)
+
+# Intances
+InstaBot(username, password)
