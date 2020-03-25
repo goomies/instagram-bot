0c5a960103324c006514dbd54f1b692a66242f36
Goomie <remy.manescau@gmail.com>
2020-03-25 18:09:48 +0100

    add follow profile with 100+ followers

--- src/Controller/BotController.php
+++ src/Controller/BotController.php
@@ -11,17 +11,9 @@ use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 class BotController extends AbstractController
 {
     /**
-     * @Route("/bot", name="bot_inddex")
+     * @Route("/bot", name="bot")
      */
-    public function botIndex()
-    {
-        return $this->render('bot/index.html.twig');
-    }
-
-    /**
-     * @Route("/mybot", name="mybot")
-     */
-    public function mybot(Request $request, ParameterBagInterface $parameterBagInterface)
+    public function bot(ParameterBagInterface $parameterBagInterface)
     {
         // Execute Chrome client
         $client = Client::createChromeClient();
@@ -32,7 +24,7 @@ class BotController extends AbstractController
         // Define username and password
         $form['username'] = $parameterBagInterface->get('app-instagram-username');
         $form['password'] = $parameterBagInterface->get('app-instagram-password');
-        
+
         // Wait for the input form element to load
         $client->waitFor("//input[@name=\"username\"]");
 
@@ -42,21 +34,60 @@ class BotController extends AbstractController
             'password' => $form['password'],
         ]);
 
-        // Wait for search bar to load on Instagram home page
+        // Wait for the search bar to load on Instagram home page
         $client->waitFor("//input[@placeholder=\"Rechercher\"]");
-        
+
         // Get search bar
         $crawler = $client->getCrawler();
         $searchbar = $crawler->filterXPath("//input[@placeholder=\"Rechercher\"]");
 
-        // Search Hashtag
+        // Search Hashtag #music
         $searchbar->sendKeys('#music');
+
+        // Wait for the results to load
         $client->waitFor('//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/div[2]/div[2]');
 
         // Open Hashtag page
-        $music = $crawler->filterXPath("//span[contains(text(), '#music')]");
-        $music->click();
+        $hashtag = $crawler->filterXPath("//span[contains(text(), '#music')]");
+        $hashtag->click();
+
+        // Wait for the Hashtag page to load
         $client->waitFor('//*[@id="react-root"]/section/main/header/div[2]/div[1]/div[1]/h1');
+
+        // Open post
+        $post = $crawler->filterXPath('//*[@id="react-root"]/section/main/article/div[1]/div/div/div[1]/div[1]/a');
+        $post->click();
+
+        // Wait for the post to load
+        $client->waitFor('/html/body/div[4]/div[2]/div/article/header/div[2]/div[1]/div[1]/a');
+
+        // Open profile
+        $profile = $crawler->filterXPath('/html/body/div[4]/div[2]/div/article/header/div[2]/div[1]/div[1]/a');
+        $profile->click();
+
+        // Wait for the profile to load
+        $client->waitFor('//*[@id="react-root"]/section/main/div/header/section/div[1]/h2');
+
+        // Get profile followers number
+        $profileFollowers = $crawler->filterXPath('//*[@id="react-root"]/section/main/div/header/section/ul/li[2]/a/span')->attr('title');
+        $profileFollowers = str_replace(' ', '', $profileFollowers);
+        $profileFollowers = intval($profileFollowers);
+
+        // Check if profile got more than 100 followers
+        if ($profileFollowers >= 100) {
+            // Follow profile
+            $followProfile = $crawler->filterXPath("//button[contains(text(), 'S’abonner')]");
+            $followProfile->click();
+        } else {
+            dump($profileFollowers);
+            die;
+        }
+
+        // Contact profile
+        // $contactProfile = $crawler->filterXPath("//button[contains(text(), 'Contacter')]");
+        // $contactProfile->click();
+
+        sleep(4);
 
         // Take screenshot
         $client->takeScreenshot('screen.png');
